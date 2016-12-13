c5d22dac9d97305b0ddb88d93e306f18510fed2a
tylernolan <trynetend@gmail.com>
2016-12-13 01:37:06 -0500

    added support for the boss stuff

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import redirect
+from flask import url_for
 import re
 import telnetlib
+import os 
+import random
 app = Flask(__name__)
-
+PATH = "C:\\Users\\Tyler\\Dropbox\\Cool Code\\GameDesignPlaysCrypt\\UnityFinalBackend\\flaskapp\\"
 #It's not often for class projects it's encouraged to enumerate all the swear words you can think of!
 #On second thought, let's source this from elsewhere...
 #Source: http://www.bannedwordlist.com/lists/swearWords.txt
@@ -17,6 +20,19 @@ def tnSend(command):
 	tn.write(command+"\r\n")
 	tn.close()
 	
+def tnPoll():
+	HOST, PORT = "localhost", 9999
+	tn = telnetlib.Telnet(HOST, port = PORT)
+	tn.write("isboss"+"\r\n")
+	received = tn.read_until("\n").strip() == "True"
+	tn.close()
+	return received
+
+def getImageFromFolder():
+	#I tried to get flask workign with local images on my windows box but it wasn't working, this is a really shitty workaround I hope to have fixed before the presentation.
+	images = ["http://i.imgur.com/2EBb9wZ.jpg", "http://i.imgur.com/OdR8lyP.png", "http://i.imgur.com/Qb3c5gf.jpg", 
+			"http://i.imgur.com/MEWNY5G.jpg", "http://i.imgur.com/Y3jpQHm.jpg"]
+	return random.choice(images) 
 	
 @app.route("/")
 def home():
@@ -35,27 +51,39 @@ def welcome(name):
 		word = word.strip()
 		r = re.compile(word, re.IGNORECASE)
 		name = r.sub("*"*len(word), name)
-		print word
 	if len(name) > 12:
 		name = name[:12] 
-	page = u'<meta name="viewport" content="width=device-width, initial-scale=1.0">'
-	page += '<form action="." method="POST">\n'
-	page += "Spawn Enemies:<br>"
-	page += u'<button name ="action" type="submit" value="{},{}">{}</button>\n'.format(str(name), "0", "Spawn Skele")
-	page += u'<button name ="action" type="submit" value="{},{}">{}</button>\n'.format(str(name), "1", "Spawn Mage")
-	page += "<br>"
-	page += "Help Player:<br>"
-	page += u'<button name ="action" type="submit" value="{},{}">{}</button>\n'.format(str(name), "2", "Heal")
-	page += u'<button name ="action" type="submit" value="{},{}">{}</button>\n'.format(str(name), "3", "Regen Mana")
-	page += u'<button name ="action" type="submit" value="{},{}">{}</button>\n'.format(str(name), "6", "Empower Weapon")
-	page += "<br>"
-	page += u'<button name ="action" type="submit" value="{},{}">{}</button>\n'.format(str(name), "7", "Give Barrier")
-	page += u'<button name ="action" type="submit" value="{},{}">{}</button>\n'.format(str(name), "4", "Smite Enemy")
-	page += "<br>"
-	page += "Other:<br>"
-	page += u'<button name ="action" type="submit" value="{},{}">{}</button>\n'.format(str(name), "5", "Say Nice Things")
-	page +='</form>'	
-	return page
+	if not tnPoll():
+		page = u'<meta name="viewport" content="width=device-width, initial-scale=1.0">'
+		page += '<form action="." method="POST">\n'
+		page += "Spawn Enemies:<br>"
+		page += u'<button name ="action" type="submit" value="{},{}">{}</button>\n'.format(str(name), "0", "Spawn Skele")
+		page += u'<button name ="action" type="submit" value="{},{}">{}</button>\n'.format(str(name), "1", "Spawn Mage")
+		page += "<br>"
+		page += "Help Player:<br>"
+		page += u'<button name ="action" type="submit" value="{},{}">{}</button>\n'.format(str(name), "2", "Heal")
+		page += u'<button name ="action" type="submit" value="{},{}">{}</button>\n'.format(str(name), "3", "Regen Mana")
+		page += u'<button name ="action" type="submit" value="{},{}">{}</button>\n'.format(str(name), "6", "Empower Weapon")
+		page += "<br>"
+		page += u'<button name ="action" type="submit" value="{},{}">{}</button>\n'.format(str(name), "7", "Give Barrier")
+		page += u'<button name ="action" type="submit" value="{},{}">{}</button>\n'.format(str(name), "4", "Smite Enemy")
+		page += "<br>"
+		page += "Other:<br>"
+		page += u'<button name ="action" type="submit" value="{},{}">{}</button>\n'.format(str(name), "5", "Say Nice Things")
+		page +='</form>'	
+		return page
+	else:
+		img = getImageFromFolder()
+		page = ""
+		page +="<!DOCTYPE html>\n"
+		page +="<html>\n"
+		page +="<body>\n"
+		page +="<h2>Give Me Your Energy!</h2>\n"
+		#page += '<img src = {} style="width:304px;height:228px;">'.format(img)
+		page += '<form action="." method="POST"><button type="submit" name="action" value="{}, -1 style="border:0;"> <img src = {} style="width:304px;height:228px;"> </button></form>'.format(name, img)
+		page +="</body>\n"
+		page +="</html>\n"
+		return page
 	
 @app.route('/welcome/', methods=["POST"])	
 def act():
@@ -67,4 +95,4 @@ def act():
 	return redirect('/welcome/{}'.format(action.split(",")[0]))
 	
 if __name__ == "__main__":
-	app.run(host= '0.0.0.0')
+	app.run(host= '0.0.0.0', debug=True)
--- twistedServer.py
+++ twistedServer.py
@@ -2,8 +2,9 @@ from twisted.internet.protocol import Factory
 from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
 class Server(LineReceiver):
-	def __init__(self, clientConn):
+	def __init__(self, clientConn, bossEnabled):
 		self.clientConn = clientConn
+		self.bossEnabled = bossEnabled
 		
 	def send_data(self, msg):
 		self.sendLine(msg + "\n")
@@ -13,14 +14,21 @@ class Server(LineReceiver):
 		if line == "supersecretpassword":
 			print "Connected to client"
 			self.clientConn[0] = self
+			self.bossEnabled[0] = False
+		elif line == "enableboss":
+			self.bossEnabled[0] = True
+		elif line == "isboss":
+			self.send_data(str(self.bossEnabled[0]))
 		elif self.clientConn[0] != None:
 			self.clientConn[0].send_data(line)
 
 class ServerFactory(Factory):
 	def __init__(self):
+		#these variables are in lists to allow me to pass them by reference into Server and maintain state. They should always only contain 1 value.
 		self.clientConn = [None]
+		self.bossEnabled = [False]
 	def buildProtocol(self, addr):
-		return Server(self.clientConn)
+		return Server(self.clientConn, self.bossEnabled)
 if __name__ == "__main__":
 	reactor.listenTCP(9999, ServerFactory())
 	reactor.run()
